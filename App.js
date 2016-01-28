a2dc8e8ab6e0f64b996a0246499db82bc1cf51f4
Liccy <latin.creature@gmail.com>
2016-01-28 17:59:18 -0500

    real time update field - "bind"state to elm based on input value

--- App.js
+++ App.js
@@ -3,31 +3,50 @@ import ReactDom from 'react-dom';
 
 // Class component can have state
 class App extends React.Component {
+    constructor() {
+        super();
+
+        this.state = { 
+            text: 'this is text',
+            cat: 0
+        }
+    }
+
+    update(e) {
+       this.setState({ text: e.target.value});  
+    }
+
     render(){
-        let txt = this.props.txt
-        return <h1>{txt}</h1>
+        // let txt = this.props.txt
+        // return <h1>{txt}</h1>
+        return (
+            <div>
+                <input type="text" onChange={this.update.bind(this)} />
+                <h1>{this.state.text}</h1>
+            </div>
+        )
     }
 }
 
 //define prp types
-//can also ser req prop.
-App.propTypes = {
-    txt: React.PropTypes.string,
-    cat: React.PropTypes.number.isRequired
-}
+// //can also ser req prop.
+// App.propTypes = {
+//     txt: React.PropTypes.string,
+//     cat: React.PropTypes.number.isRequired
+// }
 
-//set default prop values
-//can get overwritten by template
-App.defaultProps = {
-    txt: 'this is the defauklt'
-}
+// //set default prop values
+// //can get overwritten by template
+// App.defaultProps = {
+//     txt: 'this is the defauklt'
+// }
 
-//stateless function components | can't have state
-// const App = () => <h1>Hello Word</h1>
+// //stateless function components | can't have state
+// // const App = () => <h1>Hello Word</h1>
 
-ReactDom.render(
-    <App cat={5} txt="this is the props text new value" />, document.getElementById('app')
-);
+// ReactDom.render(
+//     <App cat={5} txt="this is the props text new value" />, document.getElementById('app')
+// );
 
 
-// export default App 
+export default App 
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //we can also do the setup from the app.js file
 
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
-// ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'))
